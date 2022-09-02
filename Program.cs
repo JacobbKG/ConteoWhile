943c19a6f6abb6b846239f5dbfff20458d94bf4b
Jacobb26 <fmartinezd3@miumg.edu.gt>
2022-09-02 10:51:43 -0600

    ConteoWhile

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿
+using System.Globalization;
+using System;
+
+
+internal class Program 
+{
+		static void Main(string[] args)
+    {
+        {
+            int i;
+			i = 100;
+			while (i>=0) {
+				Console.WriteLine(i);
+				i = i-1;
+			}
+		}
+      }
+}
